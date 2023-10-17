e693e485b71a13da32622485365ca60b0b12c290
MedelsteinE <medelstein1123@gmail.com>
2023-10-17 00:19:06 -0400

    joined tables to get employee by id

--- query.sql
+++ query.sql
@@ -1,11 +1,36 @@
 USE employee_tracker;
 
 -- Get employees by role_id
-SELECT
-e.first_name,
-e.last_name,
-r.title AS role_title.
-d.dep_name AS department
-FROM employees e
-    JOIN r role
-        ON e.department_id
+SELECT 
+    e.id AS employee_id, 
+    e.first_name, 
+    e.last_name, 
+    r.title AS job_title, 
+    d.name AS department_name, 
+    r.salary
+FROM 
+    employees e
+JOIN 
+    role r ON e.role_id = r.id
+JOIN 
+    department d ON r.department_id = d.id
+LEFT JOIN 
+    employees m ON e.manager_id = m.id;
+
+--Get all epmloyees 
+SELECT 
+    e.id AS employee_id, 
+    e.first_name, 
+    e.last_name, 
+    r.title AS job_title, 
+    d.name AS department_name, 
+    r.salary,
+    CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+FROM 
+    employees e
+JOIN 
+    role r ON e.role_id = r.id
+JOIN 
+    department d ON r.department_id = d.id
+LEFT JOIN 
+    employees m ON e.manager_id = m.id;
